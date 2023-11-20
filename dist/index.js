648ca409de23f6edab8d48301f16e3c6a81956cb
Muccachino <lucas_staszewski@yahoo.de>
2023-11-20 22:40:49 +0100

    first commit

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log("Hello World 5");
+//# sourceMappingURL=index.js.map
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const webpack = require("webpack");
+const path = require("path");
+
+const config = {
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  watch: true,
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              mimetype: "image/png",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: "file-loader",
+      },
+      {
+        test: /\.ts(x)?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+};
+
+module.exports = config;
