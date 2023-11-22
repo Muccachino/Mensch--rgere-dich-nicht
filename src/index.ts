4c9f0a96ddc247cfa9e9d5736fbd86b9e6d94b6c
Muccachino <lucas_staszewski@yahoo.de>
2023-11-22 13:58:59 +0100

    gameboard creation experiments

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,51 @@
+"use strict";
+
+import { doesNotMatch } from "assert";
 import "./styles.scss";
-console.log("Hello World 8");
+console.log("Hello World 10");
+
+const boardFields = [
+  1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0,
+  1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0,
+  0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0,
+  1, 1,
+];
+
+const createBoard = () => {
+  let board = document.getElementById("app");
+  boardFields.forEach((field) => {
+    let newField = createTags("div", "field", null, null);
+    expandHtml(board, newField);
+    if (field == 1) {
+      let circle: HTMLElement = createTags("div", "circle", null, null);
+      expandHtml(newField, circle);
+    }
+  });
+};
+
+createBoard();
+
+function createTags(
+  htmlTags: string,
+  className: string | null,
+  idName: string | null,
+  innerHtml: string | null
+) {
+  let element = document.createElement(htmlTags);
+  if (className != null) {
+    element.classList.add(className);
+  }
+  if (idName != null) {
+    element.id = idName;
+  }
+  if (innerHtml != null) {
+    element.innerHTML = innerHtml;
+  }
+  return element;
+}
+
+function expandHtml(expand: any, tags: any) {
+  expand.appendChild(tags);
+}
